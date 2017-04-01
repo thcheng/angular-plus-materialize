d5f8460fc5d748995b560accd79d57d8684cc2a0
Timothy Cheng <thtcheng@yahoo.com>
2017-03-31 17:17:04 -0700

    Commented out the offending import statement

--- app/components/carousel.ts
+++ app/components/carousel.ts
@@ -1,5 +1,5 @@
 import {Component, EventEmitter, ViewChild} from "@angular/core"
-import {MaterializeAction} from "angular2-materialize"
+// import {MaterializeAction} from "angular2-materialize"
 
 @Component({
     selector: "carousel",
